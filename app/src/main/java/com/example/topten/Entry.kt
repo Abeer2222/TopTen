1e4729f6582ebefeae94d5d235e12b3a0c1b425c
Abeer2222 <437804381@kku.edu.sa>
2021-10-20 17:16:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/topten/Entry.kt
@@ -0,0 +1,10 @@
+package com.example.topten
+
+class Entry {
+    var name: String = ""
+    override fun toString(): String {
+        return """
+            name = $name
+           """.trimIndent()
+    }
+}
